88a1b4f24df7800ae6b6c8fa3eff3350012adcc4
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-23 09:51:30 +0530

    Update User.controller.js

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -54,7 +54,7 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
