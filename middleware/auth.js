64f6a11bcec27ba424161eea897c591e54dd8780
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-23 14:10:15 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,40 +1,19 @@
-// import jwt from "jsonwebtoken";
-
-// export const protect = (req, res, next) => {
-//   const token = req.cookies?.token;
-
-//   if (!token) {
-//     return res.status(401).json({ message: "No token" });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
-//     return res.status(401).json({ message: "Invalid token" });
-//   }
-
-// };
-
-
 import jwt from "jsonwebtoken";
-import User from "../models/User.model.js";
 
-export const protect = async (req, res, next) => {
-  try {
-    const token = req.cookies.token;
+export const protect = (req, res, next) => {
+  const token = req.cookies?.token;
 
-    if (!token) {
-      return res.status(401).json({ message: "Not authorized" });
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = await User.findById(decoded.id).select("-password");
-
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+  if (!token) {
+    return res.status(401).json({ message: "No token" });
   }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
 };
+
