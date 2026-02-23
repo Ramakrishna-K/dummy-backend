011e0d07dcade5f3046f32f7217c7ebc442fca09
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-23 13:44:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,15 +65,29 @@ const allowedOrigins = [
   "https://dummy-frontend-7qpg.vercel.app",
   "http://localhost:5173"
 ];
-
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 /* ✅ body & cookies AFTER cors */
 app.use(express.json());
