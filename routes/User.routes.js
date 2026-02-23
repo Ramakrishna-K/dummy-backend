6841f1e02117b462c05ee430cb98bdc67d145999
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-23 14:05:09 +0530

    Update User.routes.js

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -13,6 +13,24 @@
 
 
 
+// import express from "express";
+// import {
+//   registerUser,
+//   loginUser,
+//   profileUser,
+//   logoutUser,
+// } from "../controllers/User.controller.js";
+// import { protect } from "../middleware/auth.js";
+
+// const router = express.Router();
+
+// router.post("/register", registerUser);
+// router.post("/login", loginUser);
+// router.get("/profile", protect, profileUser);
+// router.post("/logout", logoutUser);
+
+// export default router;
+
 import express from "express";
 import {
   registerUser,
@@ -27,6 +45,8 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", protect, profileUser);
-router.post("/logout", logoutUser);
+router.post("/logout", protect, logoutUser);
 
 export default router;
+
+
